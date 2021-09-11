41507bfacec750727226d9a69a79c03ee542a91f
wibumylife <49218670+wibumylife@users.noreply.github.com>
2021-09-11 08:43:38 +0700

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -534,9 +534,9 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             Telegraph(access_token=telegraph_token).edit_page(path = self.path[prev_page],
-                                 title = 'Deepak Clouds Torrent Search',
-                                 author_name='Deepak Mirror Bot',
-                                 author_url='https://t.me/cloudsmirror',
+                                 title = 'Andora-MirrorBot Search',
+                                 author_name='Andora-MirrorBot',
+                                 author_url='https://t.me/RwSZay',
                                  html_content=content)
         return
 
@@ -619,9 +619,9 @@ class GoogleDriveHelper:
 
             for content in self.telegraph_content :
                 self.path.append(Telegraph(access_token=telegraph_token).create_page(
-                                                        title = 'Deepak Clouds Torrent Search',
-                                                        author_name='Deepak Clouds Torrent',
-                                                        author_url='https://telegram.dog/cloudsmirror',
+                                                        title = 'Andora-MirrorBot Search',
+                                                        author_name='Andora-MirrorBot',
+                                                        author_url='https://telegram.dog/RwSZay',
                                                         html_content=content
                                                         )['path'])
 
